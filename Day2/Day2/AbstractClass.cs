71ca2f13df4d6751aa2d33a8f745d8e8d0e01a3d
KDH5706 <rpaehdgns12@naver.com>
2022-04-21 16:48:22 +0900

    Study_Day2

---
+++ Day2/Day2/AbstractClass.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace AbstractClass
+{
+    abstract class AbstractBase
+    {
+        protected void PrivateMethodA()
+        {
+            Console.WriteLine("AbstractBase.PrivateMethodA()");
+        }
+
+        public void PublicMethodA()
+        {
+            Console.WriteLine("AbstractBase.PublicMethodA()");
+        }
+
+        public abstract void AbstractMethodA();
+    }
+
+    class Dervied : AbstractBase
+    {
+        public override void AbstractMethodA()
+        {
+            Console.WriteLine("Dervied.PrivateMethodA()");
+            PrivateMethodA();
+        }
+    }
+
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            AbstractBase obj = new Dervied();
+            obj.AbstractMethodA();
+            obj.PublicMethodA();
+        }
+    }
+}
---
+++ Day2/Day2/DefaultImplementation.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Day2
+{
+    interface ILogger
+    {
+        void WriteLog(string message);  //ILogger를 상속받는 클래스들은 WriteLog 메소드를 무조건 정의해야한다.
+        void WriteError(string error)
+        {
+            WriteLog($"Error: {error}");
+        }
+    }
+
+    class ConsoleLogger : ILogger
+    {
+        public void WriteLog(string message)
+        {
+            Console.WriteLine(
+                $"{ DateTime.Now.ToLocalTime()}, { message}");
+        }
+    }
+
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            ILogger logger = new ConsoleLogger();
+            logger.WriteLog("System Up");
+            logger.WriteError("System Fail");
+
+            ConsoleLogger clogger = new ConsoleLogger();
+            clogger.WriteLog("System Up");
+            //clogger.WriteError("System Fail");
+        }
+    }
+}
