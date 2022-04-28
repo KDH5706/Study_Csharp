3fed3ff45be7a8339662cb77b0f5152b9dee8f3b
KDH5706 <rpaehdgns12@naver.com>
2022-04-28 17:18:58 +0900

    Study_Day7

---
+++ Day7/Day7/ConstraintsOnTypeParaneters.cs
@@ -0,0 +1,82 @@
+﻿/*
+ * where T : struct = T는 값 형식이어야 한다.
+ * where T : class = T는 참조 형식이어야 한다.
+ * where T : new() = T는 반드시 매개변수가 없는 생성자가 있어야 한다.
+ * where T : 기반_클래스_이름 = T는 명시한 기반 클래스의 파생 클래스여야 한다.
+ * where T : 인터페이스_이름 = T는 명시한 인터페이스를 반드시 구현해야 한다. (여러개의 인터페이스를 명시할 수 있다.)
+ * where T : U = T는 또 다른 형식 매개변수 U로부터 상속받는 클래스여야 한다.
+ */
+
+using System;
+
+namespace ConstraintsOnTypeParaneters
+{
+    class StructArray<T> where T : struct
+    {
+        public T[] Array { get; set; }
+        public StructArray(int size)
+        {
+            Array = new T[size];
+        }
+    }
+
+    class RefArray<T> where T : class
+    {
+        public T[] Array { get; set; }
+        public RefArray(int size)
+        {
+            Array = new T[size];
+        }
+    }
+
+    class Base { }
+    class Derived : Base { }
+    class BaseArray<U> where U : Base
+    {
+        public U[] Array { get; set; }
+        public BaseArray(int size)
+        {
+            Array = new U[size];
+        }
+
+        public void CopyArray<T>(T[] Source) where T : U
+        {
+            Source.CopyTo(Array, 0);
+        }
+    }
+
+    class MainApp
+    {
+        public static T CreateInstance<T>() where T : new()
+        {
+            return new T();
+        }
+
+        static void Main(string[] args)
+        {
+            StructArray<int> a = new StructArray<int>(3);
+            a.Array[0] = 0;
+            a.Array[1] = 1;
+            a.Array[2] = 2;
+
+            RefArray<StructArray<double>> b = new RefArray<StructArray<double>>(3);
+            b.Array[0] = new StructArray<double>(5);
+            b.Array[1] = new StructArray<double>(10);
+            b.Array[2] = new StructArray<double>(1005);
+
+            BaseArray<Base> c = new BaseArray<Base>(3);
+            c.Array[0] = new Base();
+            c.Array[1] = new Derived();
+            c.Array[2] = CreateInstance<Base>();
+
+            BaseArray<Derived> d = new BaseArray<Derived>(3);
+            d.Array[0] = new Derived();
+            d.Array[1] = CreateInstance<Derived>();
+            d.Array[2] = CreateInstance<Derived>();
+
+            BaseArray<Derived> e = new BaseArray<Derived>(3);
+            e.CopyArray<Derived>(d.Array);
+        }
+    }
+        
+}
---
+++ Day7/Day7/ExceptionFiltering.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ExceptionFiltering
+{
+    class FilterableException : Exception
+    {
+        public int ErrorNO { get; set; }
+    }
+
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Nuber Between 0~10");
+            string input = Console.ReadLine();
+            try
+            {
+                int num = Int32.Parse(input);
+
+                if (num < 0 || num > 10)
+                    throw new FilterableException() { ErrorNO = num };
+                else
+                    Console.WriteLine($"Output : {num}");
+            }
+            catch (FilterableException e) when (e.ErrorNO < 0)
+            {
+                Console.WriteLine("Negative input is not allowed");
+            }
+            catch (FilterableException e) when (e.ErrorNO > 10)
+            {
+                Console.WriteLine("Too big number is not allowed");
+            }
+        }
+    }
+}
